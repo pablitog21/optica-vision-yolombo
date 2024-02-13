67cb34217649e013c1a615139ba8cbf753dfcc6b
JuanDavidMarin <testing@vortexbird.com>
2024-02-13 14:14:21 -0500

    update readme

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+
+export interface ImageData {
+    id: number;
+    src: string;
+    text: string;
+  }
+  
+  export const imageData: ImageData[] = [
+    {
+      id: 1,
+      src: "/images/image1.jpg",
+      text: "Texto de la primera imagen",
+    },
+    {
+      id: 2,
+      src: "/images/image2.jpg",
+      text: "Texto de la segunda imagen",
+    },
+    {
+      id: 3,
+      src: "/images/image3.jpg",
+      text: "Texto de la tercera imagen",
+    },
+   
+  ];
+  
